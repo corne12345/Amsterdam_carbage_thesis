1138025627e2ef87f880aa29c5b749ef4b01354a
corne12345 <corneheijnen_1991@hotmail.com>
2020-05-05 12:44:42 +0200

    finished loading_data.py

--- Code/loading_data.py
+++ Code/loading_data.py
@@ -129,29 +129,33 @@ def load_api_data(prnt=False, subsectie=None):
 
 def get_db_afvalcluster_info():
     """
-    Function that modifies loads in data on the garbage clusters from the Postgres
-    database and modifies the resulting dataframe in a way that makes it usable
-    for future analysis
+    Load POI information from postgres DB.
+
+    Function that modifies loads in data on the garbage clusters from the
+    Postgres database and modifies the resulting dataframe in a way that makes
+    it usable for future analysis
     Returns:
-    - pandas DataFrame containing all information from the database and also the
-    added coordinates for the clusters and the type of POI
+    - pandas DataFrame containing all information from the database and also
+    the added coordinates for the clusters and the type of POI
     """
-    polygon_list = load_geodata_containers()
     db_df = get_dataframe("""SELECT *
                              FROM proj_afval_netwerk.afv_rel_nodes_poi
                              """)
     db_df['woning'] = db_df['bk_afv_rel_nodes_poi'].str.split('~')
-    db_df['cluster_x'] = db_df['woning'].apply(lambda x: x[0]).astype('float').round(0).astype('int')
-    db_df['cluster_y'] = db_df['woning'].apply(lambda x: x[1]).astype('float').round(0).astype('int')
+    db_df['cluster_x'] = db_df['woning'].apply(lambda x: x[0]).astype('float')\
+        .round(0).astype('int')
+    db_df['cluster_y'] = db_df['woning'].apply(lambda x: x[1]).astype('float')\
+        .round(0).astype('int')
     db_df['type'] = db_df['woning'].apply(lambda x: x[2])
     db_df['bag'] = db_df['woning'].apply(lambda x: x[3])
-    # db_df['uses_container'] = db_df.apply(lambda row: address_in_service_area(row['cluster_x'], row['cluster_y'], polygon_list = polygon_list), axis=1)
     db_df = db_df.drop('woning', axis=1)
     return db_df
 
 
 def get_distance_matrix():
     """
+    Load distance matrix from postgres DB.
+
     Function to get the distance matrix. It returns the distance matrix sorted
     by distance in ascending order tos make it suitable for further use.
     """
@@ -164,60 +168,93 @@ def get_distance_matrix():
 
 def create_all_households(rel_poi_df, subsectie=None):
     """
-    Function that creates a dataframe containing all households as rows
+    Create a dataframe containing all households.
+
+    This function takes as input the dataframe containing al POI's (both houses
+    as well as container clusters) and returns a dataframe containing the
+    information of all households. The optional parameter subsection determines
+    if the entirety of addresses is to be used. If not, a certain stadsdeel can
+    be returned.
     """
-    polygon_list = load_geodata_containers(subsectie = subsectie)
-    all_households = rel_poi_df[rel_poi_df['type']!='afval_cluster']
+    polygon_list = load_geodata_containers(subsectie=subsectie)
+    all_households = rel_poi_df[rel_poi_df['type'] != 'afval_cluster']
     all_households = all_households[['s1_afv_nodes', 'cluster_x', 'cluster_y']]
-    all_households['uses_container'] = all_households.apply(lambda row: address_in_service_area(row['cluster_x'], row['cluster_y'], polygon_list=polygon_list), axis=1)
+    all_households['uses_container'] = all_households\
+        .apply(lambda row: address_in_service_area(row['cluster_x'],
+                                                   row['cluster_y'],
+                                                   polygon_list=polygon_list),
+               axis=1)
     return all_households
+
 
 def create_aansluitingen(good_result, total_join, use_count=False):
     """
+    Create table showing number of users per fraction of container cluster.
+
     Function that returns dataframe aansluitingen that calculates amount of
     households per cluster and the percentage of overflow
     """
     if not use_count:
         aansluitingen = pd.DataFrame(good_result['poi_rest'].value_counts()).\
-                join(pd.DataFrame(good_result['poi_papier'].value_counts()), how='outer').\
-                join(pd.DataFrame(good_result['poi_plastic'].value_counts()), how='outer').\
-                join(pd.DataFrame(good_result['poi_glas'].value_counts()), how='outer').\
-                join(pd.DataFrame(good_result['poi_textiel'].value_counts()), how='outer')
+                join(pd.DataFrame(good_result['poi_papier'].value_counts()),
+                     how='outer').\
+                join(pd.DataFrame(good_result['poi_plastic'].value_counts()),
+                     how='outer').\
+                join(pd.DataFrame(good_result['poi_glas'].value_counts()),
+                     how='outer').\
+                join(pd.DataFrame(good_result['poi_textiel'].value_counts()),
+                     how='outer')
 
     else:
-        rest = pd.DataFrame(good_result.groupby('poi_rest')['count'].sum()).rename(columns={'count':'poi_rest'})
-        plastic = pd.DataFrame(good_result.groupby('poi_plastic')['count'].sum()).rename(columns={'count':'poi_plastic'})
-        papier = pd.DataFrame(good_result.groupby('poi_papier')['count'].sum()).rename(columns={'count':'poi_papier'})
-        glas = pd.DataFrame(good_result.groupby('poi_glas')['count'].sum()).rename(columns={'count':'poi_glas'})
-        textiel = pd.DataFrame(good_result.groupby('poi_textiel')['count'].sum()).rename(columns={'count':'poi_textiel'})
-        aansluitingen = rest.join([plastic, papier, glas, textiel], how='outer')
+        rest = pd.DataFrame(good_result.groupby('poi_rest')['count']
+                            .sum()).rename(columns={'count': 'poi_rest'})
+        plastic = pd.DataFrame(good_result.groupby('poi_plastic')['count']
+                               .sum()).rename(columns={'count': 'poi_plastic'})
+        papier = pd.DataFrame(good_result.groupby('poi_papier')['count']
+                              .sum()).rename(columns={'count': 'poi_papier'})
+        glas = pd.DataFrame(good_result.groupby('poi_glas')['count']
+                            .sum()).rename(columns={'count': 'poi_glas'})
+        textiel = pd.DataFrame(good_result.groupby('poi_textiel')['count']
+                               .sum()).rename(columns={'count': 'poi_textiel'})
+        aansluitingen = rest.join([plastic, papier, glas, textiel],
+                                  how='outer')
 
-
-    tmp_for_join = total_join[['van_s1_afv_nodes', 'rest', 'papier', 'plastic', \
-    'glas', 'textiel', 'totaal']].drop_duplicates().set_index('van_s1_afv_nodes')
+    tmp_for_join = \
+        total_join[['van_s1_afv_nodes', 'rest', 'papier', 'plastic', 'glas',
+                    'textiel', 'totaal']].drop_duplicates()\
+        .set_index('van_s1_afv_nodes')
     aansluitingen = aansluitingen.join(tmp_for_join, how='left')
 
-    aansluitingen['rest_perc'] = aansluitingen['poi_rest'] / aansluitingen['rest']
-    aansluitingen['plastic_perc'] = aansluitingen['poi_plastic'] / aansluitingen['plastic'] / 2
-    aansluitingen['papier_perc'] = aansluitingen['poi_papier'] / aansluitingen['papier'] / 2
-    aansluitingen['glas_perc'] = aansluitingen['poi_glas'] / aansluitingen['glas'] / 2
-    aansluitingen['textiel_perc'] = aansluitingen['poi_textiel'] / aansluitingen['textiel'] / 7.5
+    aansluitingen['rest_perc'] = \
+        aansluitingen['poi_rest'] / aansluitingen['rest']
+    aansluitingen['plastic_perc'] = \
+        aansluitingen['poi_plastic'] / aansluitingen['plastic'] / 2
+    aansluitingen['papier_perc'] = \
+        aansluitingen['poi_papier'] / aansluitingen['papier'] / 2
+    aansluitingen['glas_perc'] = \
+        aansluitingen['poi_glas'] / aansluitingen['glas'] / 2
+    aansluitingen['textiel_perc'] = \
+        aansluitingen['poi_textiel'] / aansluitingen['textiel'] / 7.5
 
     return aansluitingen
 
-def address_in_service_area(x, y, polygon_list = None, subsectie=None):
+
+def address_in_service_area(x, y, polygon_list=None, subsectie=None):
     """
-    function to see whether a certain household is within the service area of rest.
-    The test criterion is a shapefile containing all places in the city of
-    Amsterdam
-    Input is x and y coordinates of a house and a list of polygons of service area.
-    If polygon_list is not given, it is created within the function. This makes
-    the function more dynamic, but providing polygon_list increases speed.
+    Determine if a adress POI is within the specified service area.
+
+    function to see whether a certain household is within the service area of
+    rest. The test criterion is a shapefile containing all places in the city
+    of Amsterdam.
+    Input is x and y coordinates of a house and a list of polygons of service
+    area. If polygon_list is not given, it is created within the function. This
+    makes the function more dynamic, but providing polygon_list increases
+    speed.
     Returns boolean
     """
-    if polygon_list == None:
+    if polygon_list is None:
         polygon_list = load_geodata_containers(subsectie=subsectie)
-    point = shapely.geometry.Point(float(x),float(y))
+    point = shapely.geometry.Point(float(x), float(y))
     for polygon in polygon_list:
         if polygon.contains(point):
             return True
@@ -226,12 +263,13 @@ def address_in_service_area(x, y, polygon_list = None, subsectie=None):
 
 def distance_matrix_with_counts():
     """
+    Retrieve distance matrix including the counts per address POI.
+
     Function that tries to match table with addresses per poi with
     information and subsequently with distance matrix to give back
     a distance matrix with the amount of households per addres poi.
     """
-
-    dfob= get_dataframe("""
+    dfob = get_dataframe("""
                     SELECT bk_votpand_cluster, COUNT(*)
                     FROM proj_afval_netwerk.rel_votpand_cluster_verblijfsobject
                     GROUP BY bk_votpand_cluster
@@ -239,8 +277,10 @@ def distance_matrix_with_counts():
 
     dfob['split'] = dfob['bk_votpand_cluster'].str.split('~')
     dfob['bag'] = dfob['split'].apply(lambda x: x[0]).astype('int64')
-    dfob['x'] = dfob['split'].apply(lambda x: x[1]).astype('float').round().astype('int')
-    dfob['y'] = dfob['split'].apply(lambda x: x[2]).astype('float').round().astype('int')
+    dfob['x'] = dfob['split'].apply(lambda x: x[1]).astype('float').round()\
+        .astype('int')
+    dfob['y'] = dfob['split'].apply(lambda x: x[2]).astype('float').round().\
+        astype('int')
     dfob = dfob.drop(['split'], axis=1)
 
     df_afstandn2 = get_dataframe("""
@@ -249,17 +289,23 @@ def distance_matrix_with_counts():
                                 """)
 
     df_afstandn2['split'] = df_afstandn2['bk_afv_rel_nodes_poi'].str.split('~')
-    df_afstandn2['x'] = df_afstandn2['split'].apply(lambda x: x[0]).astype('float').round().astype('int')
-    df_afstandn2['y'] = df_afstandn2['split'].apply(lambda x: x[1]).astype('float').round().astype('int')
+    df_afstandn2['x'] = df_afstandn2['split'].apply(lambda x: x[0])\
+        .astype('float').round().astype('int')
+    df_afstandn2['y'] = df_afstandn2['split'].apply(lambda x: x[1])\
+        .astype('float').round().astype('int')
     df_afstandn2['type'] = df_afstandn2['split'].apply(lambda x: x[2])
     verblijfsobjecten = df_afstandn2[df_afstandn2['type'] != 'afval_cluster']
-    verblijfsobjecten['bag'] = verblijfsobjecten['split'].apply(lambda x: x[3]).astype('int64')
+    verblijfsobjecten['bag'] = verblijfsobjecten['split'].apply(lambda x: x[3])\
+        .astype('int64')
 
-    temp = dfob.set_index(['bag', 'x', 'y']).join(verblijfsobjecten.set_index(['bag', 'x', 'y']), how='outer').reset_index()
+    temp = dfob.set_index(['bag', 'x', 'y'])\
+        .join(verblijfsobjecten.set_index(['bag', 'x', 'y']), how='outer')\
+        .reset_index()
     df_afstandn = get_distance_matrix()
-    joined= temp.set_index('s1_afv_nodes').join(df_afstandn.set_index('naar_s1_afv_nodes'), how='outer')
+    joined = temp.set_index('s1_afv_nodes')\
+        .join(df_afstandn.set_index('naar_s1_afv_nodes'), how='outer')
     joined = joined.reset_index()[['van_s1_afv_nodes', 'index', 'afstand', 'count']].\
-             rename(columns={'index':'naar_s1_afv_nodes'}).sort_values(by='afstand').\
-             reset_index().drop(['index'],axis=1).dropna()
+        rename(columns={'index': 'naar_s1_afv_nodes'}).sort_values(by='afstand').\
+        reset_index().drop(['index'], axis=1).dropna()
 
     return joined
